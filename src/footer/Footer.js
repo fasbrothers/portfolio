ef9fcafafad02256d0133d1a01a7f113d63658ba
fasbrothers <nigmatovsanjar00@gmail.com>
2021-09-28 16:14:21 +0500

    add footer to website

---
+++ src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+    Sanjar Nigmatov 2020-{new Date().getFullYear()}. All Rights reserved ©.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
